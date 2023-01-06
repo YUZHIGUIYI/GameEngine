e51094f53d553eaa9814491a09f557125e4eb832
YUZHIGUIYI <13601462025@163.com>
2023-01-06 20:48:30 +0800

    Add ShaderLibrary, fix glShaderIDs bug in OpenGLShader::Compile function

--- Hazel/include/Hazel/Events/Event.h
+++ Hazel/include/Hazel/Events/Event.h
@@ -57,20 +57,19 @@ namespace Hazel
 
     class EventDispatcher
     {
-        template<typename T>
-        using EventFn = std::function<bool(T&)>;
     public:
         EventDispatcher(Event& event)
                 : m_Event(event)
         {
         }
 
-        template<typename T>
-        bool Dispatch(EventFn<T> func)
+        // Avoid using std::function in event dispatcher for performance
+        template<typename T, typename F>
+        bool Dispatch(const F& func)
         {
             if (m_Event.GetEventType() == T::GetStaticType())
             {
-                m_Event.Handled = func(*(T*)&m_Event);
+                m_Event.Handled = func(static_cast<T&>(m_Event));
                 return true;
             }
             return false;
--- Hazel/include/Platform/OpenGL/OpenGLShader.h
+++ Hazel/include/Platform/OpenGL/OpenGLShader.h
@@ -7,16 +7,22 @@
 #include <Hazel/Renderer/Shader.h>
 #include <glm/glm.hpp>
 
+// 
+typedef unsigned int GLenum;
+
 namespace Hazel
 {
     class OpenGLShader : public Shader
     {
     public:
-        OpenGLShader(const std::string& vertexSrc, const std::string& fragmentSrc);
+        OpenGLShader(const std::string& filepath);
+        OpenGLShader(const std::string& name, const std::string& vertexSrc, const std::string& fragmentSrc);
         virtual ~OpenGLShader();
 
         virtual void Bind() const override;
         virtual void Unbind() const override;
+
+        virtual const std::string& GetName() const override { return m_Name; }
 
         void UploadUniformInt(const std::string& name, int value);
 
@@ -27,8 +33,15 @@ namespace Hazel
 
         void UploadUniformMat3(const std::string& name, const glm::mat3& matrix);
         void UploadUniformMat4(const std::string& name, const glm::mat4& matrix);
+
+    private:
+        std::string ReadFile(const std::string& filepath);
+        std::unordered_map<GLenum, std::string> PreProcess(const std::string& source);
+        void Compile(const std::unordered_map<GLenum, std::string>& shaderSources);
+
     private:
         uint32_t m_RendererID;
+        std::string m_Name;
     };
 }
 
