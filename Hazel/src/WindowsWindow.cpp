e6e77bcbc469807cad08f7af0bac4b8f1c0c568f
YUZHIGUIYI <13601462025@163.com>
2023-01-09 22:10:17 +0800

    Log all messages with OpenGL severity notification, low, medium and high

--- Hazel/src/WindowsWindow.cpp
+++ Hazel/src/WindowsWindow.cpp
@@ -8,6 +8,7 @@
 #include <Hazel/Events/KeyEvent.h>
 #include <Hazel/Events/MouseEvent.h>
 
+#include <Hazel/Renderer/Renderer.h>
 #include <Platform/OpenGL/OpenGLContext.h>
 
 namespace Hazel
@@ -62,6 +63,10 @@ namespace Hazel
 
         {
             HZ_PROFILE_SCOPE("GLFWCreateWindow");
+#ifdef HZ_DEBUG
+            if (Renderer::GetAPI() == RendererAPI::API::OpenGL)
+                glfwWindowHint(GLFW_OPENGL_DEBUG_CONTEXT, GLFW_TRUE);
+#endif
             m_Window = glfwCreateWindow((int)props.Width, (int)props.Height, m_Data.Title.c_str(), nullptr, nullptr);
             ++s_GLFWWindowCount;
         }
