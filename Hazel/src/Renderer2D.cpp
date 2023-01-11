0295e95a4915494caa57f648b1eb7309752c0bc5
YUZHIGUIYI <13601462025@163.com>
2023-01-11 22:39:41 +0800

    Add draw call splitting and statistics in Renderer2D API

--- Hazel/src/Renderer2D.cpp
+++ Hazel/src/Renderer2D.cpp
@@ -23,9 +23,9 @@ namespace Hazel
 
     struct Renderer2DData
     {
-        const uint32_t MaxQuads = 10000;
-        const uint32_t MaxVertices = MaxQuads * 4;
-        const uint32_t MaxIndices = MaxQuads * 6;
+        static const uint32_t MaxQuads = 20000;
+        static const uint32_t MaxVertices = MaxQuads * 4;
+        static const uint32_t MaxIndices = MaxQuads * 6;
         static const uint32_t MaxTextureSlots = 32; // ToDo: RenderCaps
 
         Ref<VertexArray> QuadVertexArray;
@@ -41,6 +41,8 @@ namespace Hazel
         uint32_t TextureSlotIndex = 1;  // 0 = white texture
 
         glm::vec4 QuadVertexPositions[4];
+
+        Renderer2D::Statistics Stats;
     };
 
     static Renderer2DData s_Data;
@@ -144,6 +146,18 @@ namespace Hazel
         }
 
         RenderCommand::DrawIndexed(s_Data.QuadVertexArray, s_Data.QuadIndexCount);
+
+        ++s_Data.Stats.DrawCalls;
+    }
+
+    void Renderer2D::FlushAndReset()
+    {
+        EndScene();
+
+        s_Data.QuadIndexCount = 0;
+        s_Data.QuadVertexBufferPtr = s_Data.QuadVertexBufferBase;
+
+        s_Data.TextureSlotIndex = 1;
     }
 
     void Renderer2D::DrawQuad(const glm::vec2 &position, const glm::vec2 &size, const glm::vec4 &color)
@@ -154,6 +168,11 @@ namespace Hazel
     void Renderer2D::DrawQuad(const glm::vec3 &position, const glm::vec2 &size, const glm::vec4 &color)
     {
         HZ_PROFILE_FUNCTION();
+
+        if (s_Data.QuadIndexCount >= Renderer2DData::MaxIndices)
+        {
+            FlushAndReset();
+        }
 
         const float textureIndex = 0.0f;    // White Texture
         const float tilingFactor = 1.0f;
@@ -190,6 +209,8 @@ namespace Hazel
         s_Data.QuadVertexBufferPtr++;
 
         s_Data.QuadIndexCount += 6;
+
+        ++s_Data.Stats.QuadCount;
     }
 
     void Renderer2D::DrawQuad(const glm::vec2 &position, const glm::vec2 &size, const Ref<Texture2D> &texture,
@@ -202,6 +223,11 @@ namespace Hazel
                             float tilingFactor, const glm::vec4& tintColor)
     {
         HZ_PROFILE_FUNCTION();
+
+        if (s_Data.QuadIndexCount >= Renderer2DData::MaxIndices)
+        {
+            FlushAndReset();
+        }
 
         constexpr glm::vec4 color = { 1.0f, 1.0f, 1.0f, 1.0f };
 
@@ -254,6 +280,8 @@ namespace Hazel
         s_Data.QuadVertexBufferPtr++;
 
         s_Data.QuadIndexCount += 6;
+
+        ++s_Data.Stats.QuadCount;
     }
 
     void Renderer2D::DrawRotatedQuad(const glm::vec2 &position, const glm::vec2 &size, float rotation, const glm::vec4 &color)
@@ -264,6 +292,11 @@ namespace Hazel
     void Renderer2D::DrawRotatedQuad(const glm::vec3 &position, const glm::vec2 &size, float rotation, const glm::vec4 &color)
     {
         HZ_PROFILE_FUNCTION();
+
+        if (s_Data.QuadIndexCount >= Renderer2DData::MaxIndices)
+        {
+            FlushAndReset();
+        }
 
         const float textureIndex = 0.0f;    // White texture
         const float tilingFactor = 1.0f;
@@ -302,6 +335,7 @@ namespace Hazel
 
         s_Data.QuadIndexCount += 6;
 
+        ++s_Data.Stats.QuadCount;
     }
 
     void Renderer2D::DrawRotatedQuad(const glm::vec2 &position, const glm::vec2 &size, float rotation, const Ref<Texture2D> &texture,
@@ -314,6 +348,11 @@ namespace Hazel
                                     float tilingFactor, const glm::vec4 &tintColor)
     {
         HZ_PROFILE_FUNCTION();
+
+        if (s_Data.QuadIndexCount >= Renderer2DData::MaxIndices)
+        {
+            FlushAndReset();
+        }
 
         constexpr glm::vec4 color = { 1.0f, 1.0f, 1.0f, 1.0f };
 
@@ -367,6 +406,18 @@ namespace Hazel
         s_Data.QuadVertexBufferPtr++;
 
         s_Data.QuadIndexCount += 6;
+
+        ++s_Data.Stats.QuadCount;
+    }
+
+    void Renderer2D::ResetStats()
+    {
+        std::memset(&s_Data.Stats, 0, sizeof(Statistics));
+    }
+
+    Renderer2D::Statistics Renderer2D::GetStats()
+    {
+        return s_Data.Stats;
     }
 }
 
