b71f228582215b26be5ef06d1a7818ec879c21a8
YUZHIGUIYI <13601462025@163.com>
2023-01-22 16:09:25 +0800

    Add EventCategoryMouseButton into MouseButtonEvent

--- Hazel/include/Hazel/Events/MouseEvent.h
+++ Hazel/include/Hazel/Events/MouseEvent.h
@@ -59,7 +59,7 @@ namespace Hazel
     public:
         inline MouseCode GetMouseButton() const { return m_Button; }
 
-        EVENT_CLASS_CATEGORY(EventCategoryMouse | EventCategoryInput)
+        EVENT_CLASS_CATEGORY(EventCategoryMouse | EventCategoryInput | EventCategoryMouseButton)
     protected:
         MouseButtonEvent(const MouseCode button)
                 : m_Button(button) {}
