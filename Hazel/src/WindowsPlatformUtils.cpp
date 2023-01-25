3e3ca99f637321355eeb2da9656cc2b0d06121d6
YUZHIGUIYI <13601462025@163.com>
2023-01-25 22:59:12 +0800

    Add default extension string when saving a scene file

--- Hazel/src/WindowsPlatformUtils.cpp
+++ Hazel/src/WindowsPlatformUtils.cpp
@@ -44,6 +44,10 @@ namespace Hazel
         ofn.nMaxFile = sizeof(szFile);
         ofn.lpstrFilter = filter;
         ofn.nFilterIndex = 1;
+
+        // Set the default extension by extracting it from the filter
+        ofn.lpstrDefExt = std::strchr(filter, '\0') + 1;    // TODO
+
         ofn.Flags = OFN_PATHMUSTEXIST | OFN_FILEMUSTEXIST | OFN_NOCHANGEDIR;
         if (GetSaveFileNameA(&ofn) == TRUE)
         {
