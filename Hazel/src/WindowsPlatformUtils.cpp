2f14f2f70682fbe6a884dd744b7ebb04b427429f
YUZHIGUIYI <13601462025@163.com>
2023-01-26 21:40:50 +0800

    Make file dialogs start from current working directory

--- Hazel/src/WindowsPlatformUtils.cpp
+++ Hazel/src/WindowsPlatformUtils.cpp
@@ -19,11 +19,14 @@ namespace Hazel
     {
         OPENFILENAMEA ofn;
         CHAR szFile[260] = { 0 };
+        CHAR currentDir[256] = { 0 };
         ZeroMemory(&ofn, sizeof(OPENFILENAME));
         ofn.lStructSize = sizeof(OPENFILENAME);
         ofn.hwndOwner = glfwGetWin32Window((GLFWwindow*)Application::Get().GetWindow().GetNativeWindow());
         ofn.lpstrFile = szFile;
         ofn.nMaxFile = sizeof(szFile);
+        if (GetCurrentDirectoryA(256, currentDir))
+            ofn.lpstrInitialDir = currentDir;
         ofn.lpstrFilter = filter;
         ofn.nFilterIndex = 1;
         ofn.Flags = OFN_PATHMUSTEXIST | OFN_FILEMUSTEXIST | OFN_NOCHANGEDIR;
@@ -37,14 +40,17 @@ namespace Hazel
     {
         OPENFILENAMEA ofn;
         CHAR szFile[260] = { 0 };
+        CHAR currentDir[256] = { 0 };
         ZeroMemory(&ofn, sizeof(OPENFILENAME));
         ofn.lStructSize = sizeof(OPENFILENAME);
         ofn.hwndOwner = glfwGetWin32Window((GLFWwindow*)Application::Get().GetWindow().GetNativeWindow());
         ofn.lpstrFile = szFile;
         ofn.nMaxFile = sizeof(szFile);
+        if (GetCurrentDirectoryA(256, currentDir))
+            ofn.lpstrInitialDir = currentDir;
         ofn.lpstrFilter = filter;
         ofn.nFilterIndex = 1;
-        ofn.Flags = OFN_PATHMUSTEXIST | OFN_FILEMUSTEXIST | OFN_NOCHANGEDIR;
+        ofn.Flags = OFN_PATHMUSTEXIST | OFN_OVERWRITEPROMPT | OFN_NOCHANGEDIR;
 
         // Set the default extension by extracting it from the filter
         ofn.lpstrDefExt = std::strchr(filter, '\0') + 1;    // TODO
