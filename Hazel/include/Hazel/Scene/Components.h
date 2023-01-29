59affe46ac1aaf1780d7eea85fd921d237cff7af
YUZHIGUIYI <13601462025@163.com>
2023-01-29 23:04:40 +0800

    Add texture drag/drop and tiling factor to Sprite Component

--- Hazel/include/Hazel/Scene/Components.h
+++ Hazel/include/Hazel/Scene/Components.h
@@ -11,6 +11,7 @@
 
 #include <Hazel/Scene/SceneCamera.h>
 #include <Hazel/Scene/ScriptableEntity.h>
+#include <Hazel/Renderer/Texture.h>
 
 namespace Hazel
 {
@@ -48,6 +49,8 @@ namespace Hazel
     struct SpriteRendererComponent
     {
         glm::vec4 Color{ 1.0f, 1.0f, 1.0f, 1.0f };
+        Ref<Texture2D> Texture;
+        float TilingFactor = 1.0f;
 
         SpriteRendererComponent() = default;
         SpriteRendererComponent(const SpriteRendererComponent&) = default;
