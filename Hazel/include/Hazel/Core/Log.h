2a8a0b6c7a640a1e5f929bda012c66c78362f975
YUZHIGUIYI <13601462025@163.com>
2023-01-30 18:00:23 +0800

    Fix typo quaternio -> quaternion in Log.h

--- Hazel/include/Hazel/Core/Log.h
+++ Hazel/include/Hazel/Core/Log.h
@@ -4,10 +4,11 @@
 
 #pragma once
 
+#include <Hazel/Core/Base.h>
+
 #define GLM_ENABLE_EXPERIMENTAL
 #include <glm/gtx/string_cast.hpp>
 
-#include <Hazel/Core/Base.h>
 // This ignores all warnings raised inside external headers
 #pragma warning(push, 0)
 #include <spdlog/spdlog.h>
@@ -43,9 +44,9 @@ inline OStream& operator<<(OStream& os, const glm::mat<C, R, T, Q>& matrix)
 }
 
 template<typename OStream, typename T, glm::qualifier Q>
-inline OStream& operator<<(OStream& os, glm::qua<T, Q> quaternio)
+inline OStream& operator<<(OStream& os, glm::qua<T, Q> quaternion)
 {
-    return os << glm::to_string(quaternio);
+    return os << glm::to_string(quaternion);
 }
 
 // Core log macros
